46b345049fc4ca5f9540298b681a64b0319c771b
Devendra <bhaijedevendra@gmail.com>
2026-08-10 11:27:09 +0530

    feat: react props

--- react-notes/src/App.jsx
+++ react-notes/src/App.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import * as Components from "./Phase-1-Foundations/Module-03-Fundamentals/01-components";
+import * as Jsx from "./Phase-1-Foundations/Module-03-Fundamentals/02-jsx-and-elements";
+import * as Props from "./Phase-1-Foundations/Module-03-Fundamentals/03-props";
 
 const App = () => {
   return (
-    <div>
+    <>
       {/* <Components.ComponentsExamples /> */}
       {/* <Components.ExportsExamples /> */}
-      <Components.CompositionExamples />
-    </div>
+      {/* <Components.CompositionExamples /> */}
+
+      {/* <Jsx.JsxBasics /> */}
+      {/* <Jsx.Fragments /> */}
+      {/* <Jsx.Styling /> */}
+
+      {/* <Props.PropsBasics /> */}
+      {/* <Props.ChildrenProp /> */}
+      <Props.PropDrilling />
+    </>
   );
 };
 
